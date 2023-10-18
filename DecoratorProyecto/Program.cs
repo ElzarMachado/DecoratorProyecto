9711341e27c26ca85d70467b9d4e2478e396a840
ElzarMachado <101478824+ElzarMachado@users.noreply.github.com>
2023-10-18 06:28:21 -0700

    Explicacion de lo que entendi del patron

--- DecoratorProyecto/Program.cs
+++ DecoratorProyecto/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//El decorator es un patron de diseño que asicamente lo que hace es que permite agregar lo que puede hacer un objeto sin que se tenga que cambiar la estrucutra
+//este es diferente a la herencia en la herencia se la clase  debe depender de la clase padre y en el decorator no en el decorator se agrega una clase que se encarga de agregar las funcionalidades
+
+
+
